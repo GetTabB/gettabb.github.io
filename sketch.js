afb19785a3b4730121f4241a47f25e47a8fab869
Kai W <kai@thing.city>
2023-12-05 18:19:32 +0000

    Update index.html, script.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,10 @@
+let workingSites = []
+let playingSites = []
+
 function setup() {
+  createCanvas(windowWidth, windowWidth);
 }
 
 function draw() {
-  createCanvas(windowWidth, windowHeight);
   background(220);
-  
-  if(width > height) {
-    textSize(height / 32)
-  } else {
-    textSize(width / 32)
-  }
-  
-  textAlign(CENTER,CENTER)
-  
-  text("hello", width / 2, height / 2)
 }
