5ef0f30e521850cc24d14a881d12e1efaa08c229
Kai W <kai@thing.city>
2023-12-05 18:23:50 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,15 @@
-let workingSites = []
-let playingSites = []
+let currentlyDoing = 'request'
+
+let workingSites = ['stackoverflow.com','github.com']
+let playingSites = ['scratch.mit.edu','youtube.com']
 
 function setup() {
   createCanvas(windowWidth, windowWidth);
 }
 
 function draw() {
-  background(220);
+  background(255)
+  text('currentlyDoing: ' + currentlyDoing, 10, 20)
+  text('workingSites: ' + workingSites, 10, 40)
+  text('playingSites: ' + playingSites, 10, 60)
 }
